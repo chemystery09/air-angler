7a266542c8768b7521936d39b2a8658c936f2af7
aedan <118068380+AedanStewart@users.noreply.github.com>
2025-03-29 23:52:50 -0400

    Co-authored-by: Holly Xue <hollyxue05@users.noreply.github.com> Co-authored-by: WorseCpp <WorseCpp@users.noreply.github.com>

--- src/rod.py
+++ src/rod.py
@@ -1,9 +1,13 @@
 import os
+import random
 from pathlib import Path
 
 import pygame
 
-#main_dir = os.path.split(Path.resolve(__file__))[0]
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+WIDTH, HEIGHT = 1920, 1080
+MIN_FISH, MAX_FISH = 10, 20
 
 
 class GameObject:
@@ -34,9 +38,9 @@ class GameObject:
         #     self.pos.top = HEIGHT - SPRITE_HEIGHT
 
 
-#def load_image(name):
-#    path = os.path.join(main_dir, "data", name)
-#    return pygame.image.load(path).convert()
+def load_image(name):
+    path = os.path.join(main_dir, "data", name)
+    return pygame.image.load(path).convert()
 
 
 class Rod(GameObject):
@@ -44,4 +48,55 @@ class Rod(GameObject):
         super(*args, **kwargs)
         self.is_dropping = False
 
-        
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    fish1_img = load_image("fish1.png")
+    fish2_img = load_image("fish2.png")
+    fish3_img = load_image("fish3.png")
+
+    # background = load_image("allfish.png")  # change me!
+
+    # background = pygame.transform.scale2x(background)
+    # background = pygame.transform.scale2x(background)
+
+    # screen.blit(background, (0, 0))
+
+    fish_objects = []
+
+    for _ in range(random.randint(MIN_FISH, MAX_FISH)):
+        img = random.choice((fish1_img, fish2_img, fish3_img))
+        x = random.randint(0, 10)
+        fish_objects.append(GameObject(img, x * 40, x))
+
+    chosen_fish_tmp = random.choice(fish_objects)
+    while True:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            chosen_fish_tmp.move(up=True)
+        if keys[pygame.K_DOWN]:
+            chosen_fish_tmp.move(down=True)
+        if keys[pygame.K_LEFT]:
+            chosen_fish_tmp.move(left=True)
+        if keys[pygame.K_RIGHT]:
+            chosen_fish_tmp.move(right=True)
+
+        # screen.blit(background, (0, 0))
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                return
+        # for o in fish_objects:
+        #     screen.blit(background, o.pos)
+        for o in fish_objects:
+            screen.blit(o.image, o.pos)
+        clock.tick(60)
+        pygame.display.update()
+        pygame.time.delay(100)
+
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
